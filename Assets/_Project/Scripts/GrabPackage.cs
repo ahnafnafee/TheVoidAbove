c7eea060fde0d8f4a6ad234d6acb15517b37c11b
Ahnaf An Nafee <nafee01@gmail.com>
2020-11-20 17:09:46 -0500

    Grab fix

--- Assets/_Project/Scripts/GrabPackage.cs
+++ Assets/_Project/Scripts/GrabPackage.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class GrabPackage : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     private GameObject grabUI;
     PlayerControls _playerControls;
     private bool inRange;
@@ -18,10 +18,11 @@ public class GrabPackage : MonoBehaviour
         grabUI.SetActive(false);
     }
 
+
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(inRange)
+        if (inRange)
         {
             grabUI.SetActive(true);
         }
@@ -30,22 +31,35 @@ public class GrabPackage : MonoBehaviour
             grabUI.SetActive(false);
         }
         PlayerControls.PlayerStandardActions actions = _playerControls.PlayerStandard;
+
+        if (actions.PickUp.triggered)
+        {
+            Debug.Log("E Pressed");
+        }
+
         if (inRange && actions.PickUp.triggered)
         {
+            Debug.Log("Picked Up");
             GameObject.FindGameObjectWithTag("Player").transform.GetComponent<PackageManager>().pickUp();
             grabUI.SetActive(false);
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.tag.Equals("Player"))
+        if (other.tag.Equals("Player"))
+        {
             inRange = true;
+            Debug.Log("Enter");
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag.Equals("Player"))
+        {
             inRange = false;
+            Debug.Log("Exit");
+        }
     }
 }
