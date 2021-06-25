aca0032c3aafb8979497742d4812f71514198077
Ahnaf An Nafee <48362866+ahnafnafee@users.noreply.github.com>
2021-06-25 14:47:32 -0400

    rnd stuff

---
+++ Assets/_Project/Scripts/Application/Editor/ReleaseEditorMemory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ReleaseEditorMemory
+{
+    [MenuItem("Tools/PHL/Release Editor Memory")]
+    public static void Release()
+    {
+        EditorUtility.UnloadUnusedAssetsImmediate(true);
+        EditorUtility.UnloadUnusedAssetsImmediate(false);
+        Resources.UnloadUnusedAssets();
+        System.GC.Collect();
+    }
+}
---
+++ Assets/_Project/Scripts/Application/Editor/ReplaceSelection.cs
@@ -0,0 +1,90 @@
+﻿#pragma warning disable
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class ReplaceSelection : ScriptableWizard
+{
+	static GameObject replacement = null;
+	static bool keep = false;
+	
+	public GameObject ReplacementObject = null;
+	public bool KeepOriginals = false;
+	
+	[MenuItem("Tools/PHL/Replace Selection")]
+	static void CreateWizard()
+	{
+		ScriptableWizard.DisplayWizard("Replace Selection", typeof(ReplaceSelection), "Replace");
+	}
+	
+	public ReplaceSelection()
+	{
+		ReplacementObject = replacement;
+		KeepOriginals = keep;
+	}
+	
+	void OnWizardUpdate()
+	{
+		replacement = ReplacementObject;
+		keep = KeepOriginals;
+	}
+	
+	void OnWizardCreate()
+	{
+        if (replacement == null)
+        {
+            return;
+        }
+
+		Undo.RegisterSceneUndo("Replace Selection");
+		
+		Transform[] transforms = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
+		
+		foreach (Transform t in transforms)
+		{
+            GameObject g = null;
+			PrefabType pref = EditorUtility.GetPrefabType(replacement);
+			
+			if (pref == PrefabType.Prefab || pref == PrefabType.ModelPrefab)
+			{
+                if(t.parent == null)
+                {
+                    g = (GameObject)PrefabUtility.InstantiatePrefab(replacement, t.gameObject.scene);
+                }
+                else
+                {
+                    g = (GameObject)PrefabUtility.InstantiatePrefab(replacement, t.parent);
+                }
+			}
+			else
+			{
+                if (t.transform.parent == null)
+                {
+                    g = (GameObject)Editor.Instantiate(replacement);
+                    EditorSceneManager.MoveGameObjectToScene(g, t.gameObject.scene);
+                }
+                else
+                {
+                    g = (GameObject)Editor.Instantiate(replacement, t.parent);
+                }
+			}
+			
+			Transform gTransform = g.transform;
+			gTransform.parent = t.parent;
+			g.name = replacement.name;
+			gTransform.localPosition = t.localPosition;
+			gTransform.localScale = t.localScale;
+			gTransform.localRotation = t.localRotation;
+		}
+		
+		if (!keep)
+		{
+			foreach (GameObject g in Selection.gameObjects)
+			{
+				GameObject.DestroyImmediate(g);
+			}
+		}
+	}
+}
